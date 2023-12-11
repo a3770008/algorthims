77ad1696e18893499aa5a2630dd9f06d312e88a5
a3770008 <a3770008@yahoo.com.tw>
2023-12-11 21:07:41 +0800

    test project

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a[5],count = 5;
+	int i = 0, j = 0;
+
+	while (1)
+	{
+		if (i == 5)
+			break;
+		printf("input num : ");
+		scanf("%d", &a[i]);
+		i++;
+	}
+
+	for (i = 0; i < 5; i++)
+		printf("%d\n", a[i]);
+
+	printf("take numbr : ");
+	scanf("%d", &j);
+
+
+	for (i = 0; i < count; i++)
+	{ 
+		if(i >= count-j)
+			printf("%d\n", a[i]);
+	}
+
+	system("pause");
+	return 0;
+}
